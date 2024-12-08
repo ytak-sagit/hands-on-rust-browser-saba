c0ddca0ac63b3bb1d89c851058bf4011d875d6ab
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-12-08 10:34:07 +0000

    feat: `saba`プロジェクトを追加

---
+++ saba/src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![cfg_attr(not(target_os = "linux"), no_main)]
+
+use noli::{
+    entry_point,
+    prelude::{Api, SystemApi},
+    println,
+};
+
+fn main() {
+    Api::write_string("Hello World\n");
+    println!("Hello from println!");
+    Api::exit(42);
+}
+
+entry_point!(main);
